22ab0d7b90d644747f644b14c541d5df30b1bf6e
alexssander-01 <alexssander.pereira08@gmail.com>
2022-07-09 22:36:17 -0300

    initial commit

---
+++ src/main/java/com/br/alexssander/evaluationproject/EvaluationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.br.alexssander.evaluationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/alexssander/evaluationproject/EvaluationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alexssander.evaluationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
